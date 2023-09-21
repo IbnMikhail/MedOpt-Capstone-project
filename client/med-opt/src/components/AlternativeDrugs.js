a3811c9b6c1fde0a8706d7f0e0fecc494446a8e3
Meedo365 <nuhuahmed365@gmail.com>
2023-09-21 23:21:00 +0100

    Update AlternativeDrugs.js

--- client/med-opt/src/components/AlternativeDrugs.js
+++ client/med-opt/src/components/AlternativeDrugs.js
@@ -1,30 +1,28 @@
-const AlternativeDrugs = () => {
+const AlternativeDrugs = ({price,name,brand,description,ingredients}) => {
   return (
     <div>
       <div className="mb-4">
         <a href="#" className="block md:flex md:items-center">
           <div className="md:w-1/4">
             <p className="w-full h-[8rem] flex justify-center items-center bg-#47d1cc text-3xl font-bold ">
-              $2000
+            &#8358; {price}
             </p>
           </div>
           <div className="md:w-3/4 md:ml-4">
-            <h3 className="text-xl font-semibold mb-2">Product Name: Power Thirst</h3>           
+            <h3 className="text-xl font-semibold mb-2">Product Name: {name}</h3>           
             <p className="text-gray-600 mb-2">
               {" "}
               <b>Brand:</b>
-              Cras nisi dolor, tristique id vehicula vitae
+              {brand}
             </p>
             <p className="text-gray-600">
               {" "}
               <b>Active Ingredients:</b>
-              Cras nisi dolor, tristique id vehicula vitae
+              {ingredients}
             </p>
             <p className="text-gray-600">
               <b>Description:</b>
-              Cras nisi dolor, tristique id vehicula vitae, mollis at eros. Ut
-              euismod sem eu tellus vestibulum, in facilisis est feugiat. Mauris
-              sed leo sed erat vestibulum suscipit.
+              {description}
             </p>
           </div>
         </a>
