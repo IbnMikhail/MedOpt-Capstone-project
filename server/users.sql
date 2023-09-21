5653bf2e77fc73f346b5a6eb0cb39d427d963b53
Meedo365 <nuhuahmed365@gmail.com>
2023-09-21 22:12:46 +0100

    Update users.sql

--- server/users.sql
+++ server/users.sql
@@ -1,10 +1,21 @@
+CREATE TABLE IF NOT EXISTS history (
+  id SERIAL PRIMARY KEY,
+  drug_id INTEGER REFERENCES drugs(id),
+  user_id INTEGER REFERENCES users(id),
+  search VARCHAR(255),
+  results JSON
+  created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  updated_at TIMESTAMP DEFAULT NOW() NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   firstname VARCHAR(255),
   lastname VARCHAR(255),
-  med_history JSON,
-  email VARCHAR(255),
+  med_history TEXT, 
+  email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255),
   created_at TIMESTAMP DEFAULT NOW() NOT NULL,
-  updated_at TIMESTAMP DEFAULT NOW() NOT NULL
+  updated_at TIMESTAMP DEFAULT NOW() NOT NULL,
+  onlineStatus SMALLINT DEFAULT 0
 );
